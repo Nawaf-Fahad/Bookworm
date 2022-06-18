fdf254671c25c1150f100e8ab52b4c42ddba87b1
Nawaf <63883911+Nawaf-Fahad@users.noreply.github.com>
2022-06-18 14:46:19 +0300

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Nawaf Alotaibi on 18/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
